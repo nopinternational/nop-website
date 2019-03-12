1eef39dbc69dc6dbb4eb1d6e122b7376df268e07
NoP ops <nopops@nightofpassion.se>
2019-03-12 22:31:32 +0100

    copy changes
    adding fest page
    
    adding fest info
    
    adding fest info to first page
    
    hr-fix
    
    typo
    
    typo
    
    adding info about fest
    
    material ui first
    
    adding party teaser
    
    fix h5-child-nesting problem
    
    fixes
    
    firebase works, but duplicate application

---
+++ src/views/PartyPage/PartyPage.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+
+import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+
+// Sections for this page
+import AboutNoP from "./Sections/AboutNoP.jsx";
+import SpringParty from "./Sections/SpringParty.jsx";
+import BecomeAMember from "./Sections/BecomeAMember.jsx";
+import WhosBehind from "./Sections/WhosBehind.jsx";
+import ProductSection from "./Sections/ProductSection.jsx";
+import TeamSection from "./Sections/TeamSection.jsx";
+import WorkSection from "./Sections/WorkSection.jsx";
+
+const dashboardRoutes = [];
+
+class PartyPage extends React.Component {
+  render() {
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Night of Passion"
+          //rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("assets/img/zero.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>Night of Passion</h1>
+                <h4>
+                  Socialt. Passion. Sex.
+                </h4>
+
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            <SpringParty />
+            <BecomeAMember />
+            
+          {/*
+            <ProductSection />
+            <TeamSection />
+            <WorkSection />
+            */}
+          </div>
+        </div>
+        {/*
+        <Footer />
+        */}
+      </div>
+    );
+  }
+}
+
+export default withStyles(landingPageStyle)(PartyPage);
---
+++ src/views/SignupPage/Sections/BecomeAMemberForm.jsx
@@ -0,0 +1,224 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+// @material-ui/icons
+import Chat from "@material-ui/icons/Chat";
+import VerifiedUser from "@material-ui/icons/VerifiedUser";
+import Fingerprint from "@material-ui/icons/Fingerprint";
+import Email from "@material-ui/icons/Email";
+import People from "@material-ui/icons/People";
+import Message from "@material-ui/icons/Message";
+
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import InfoArea from "components/InfoArea/InfoArea.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+import productStyle from "assets/jss/material-kit-react/views/landingPageSections/productStyle.jsx";
+
+import * as firebase from 'firebase';
+
+class AboutNoP extends React.Component {
+
+  state = {
+    dataSent: false,
+    open: false,
+    name: '',
+    email: '',
+    message: ''
+  };
+
+
+
+  componentDidMount() {
+    const   config = {
+      apiKey: "AIzaSyBvsvqTM8uSO60h54ZnDM2HuBb6OaN1LWs",
+      authDomain: "ultra-guard-182606.firebaseapp.com",
+      databaseURL: "https://ultra-guard-182606.firebaseio.com",
+      projectId: "ultra-guard-182606",
+      storageBucket: "ultra-guard-182606.appspot.com",
+      messagingSenderId: "928295750233"
+    };
+    firebase.initializeApp(config);
+  }
+
+
+  handleNameChange = (event) => {
+    this.setState({name: event.target.value})
+  };
+
+  handleEmailChange = (event) => {
+    this.setState({email: event.target.value})
+  };
+
+  handleMessageChange = (event) => {
+    this.setState({message: event.target.value})
+  };
+
+  
+  handleBecomeMember = () => {
+    this.setState({
+      open: false,
+    });
+    console.log('handleBecomeMember');
+    console.log(this.state);
+    this.writeUserData (this.state.email,this.state.email,this.state.message);
+  };
+
+  writeUserData = (name, email, message) => {
+    console.log('name: ', name)
+    console.log('email: ', email)
+    console.log('msg: ', message )
+    firebase.database().ref('users').push().set({
+      username: name,
+      email,
+      message,
+      created: new Date().toISOString()
+    });
+    this.setState({dataSent: true})
+  }
+
+  render() {
+    console.log('render')
+    const dataSent = this.state.dataSent;
+    
+    console.log('dataSent:', dataSent)
+    const { classes } = this.props;
+
+    if (dataSent) {
+      console.log('renderThanks')
+      
+      return (
+        <div className={classes.section}>
+        <GridContainer justify="center">
+          <GridItem xs={12} sm={12} md={8}>
+            <h2 className={classes.title}>Bli medlem</h2>
+            <h5 className={classes.description}>
+            Tack så mycket, nu är du med :)
+            </h5>
+            <Link to={"/"} className={classes.link}>
+              <Button
+              color="primary">
+                OK
+              </Button>
+            </Link>
+          </GridItem>
+        </GridContainer>
+        
+        </div>
+      )
+    }
+    else {
+      console.log('renderForm')
+      return (
+        <div className={classes.section}>
+        <GridContainer justify="center">
+          <GridItem xs={12} sm={12} md={8}>
+            <h2 className={classes.title}>Bli medlem</h2>
+            <h5 className={classes.description}>
+              Nätverket Night of Passion är en exklusiv medlemsklubb. Vi tar emot par som medlemmar som är seriösa och som delar nätverkets värderingar.
+            </h5>
+            <h5 className={classes.description}>
+              Nedan kan ni ansöka om att bli medlemmar. Efter att ni har ansökt om medlemskap kommer vi att kontakta er.
+            </h5>
+
+            <form id="signup-form" >
+
+              <CustomInput
+                labelText="Namn"
+                id="material"
+                formControlProps={{
+                  fullWidth: true
+                }}
+
+                inputProps={{
+                  inputProps:  {onChange: this.handleNameChange},
+                  type: "text",
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <People className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  )
+                }}
+              />
+              <CustomInput
+                labelText="Epost adress"
+                id="material"
+                formControlProps={{
+                  fullWidth: true
+                }}
+                inputProps={{
+                  inputProps:  {onChange: this.handleEmailChange},
+                  type: "email",
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Email className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  )
+                }}
+              />
+              <CustomInput
+                labelText="Meddelande, kod"
+                id="material"
+                formControlProps={{
+                  fullWidth: true
+                }}
+                inputProps={{
+                  inputProps:  {onChange: this.handleMessageChange},
+                  type: "text",
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Message className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  )
+                }}
+              />
+              <Button  type="button" color="primary" size="lg" onClick={this.handleBecomeMember}>
+                Bli medlem!
+              </Button>
+            </form>
+          </GridItem>
+        </GridContainer>
+          {/*
+          <div>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={4}>
+                <InfoArea
+                  title="Free Chat"
+                  description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+                  icon={Chat}
+                  iconColor="info"
+                  vertical
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
+                <InfoArea
+                  title="Verified Users"
+                  description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+                  icon={VerifiedUser}
+                  iconColor="success"
+                  vertical
+                />
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
+                <InfoArea
+                  title="Fingerprint"
+                  description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+                  icon={Fingerprint}
+                  iconColor="danger"
+                  vertical
+                />
+              </GridItem>
+            </GridContainer>
+          </div>
+          */}
+        </div>
+      );
+    }
+  }
+}
+
+export default withStyles(productStyle)(AboutNoP);
