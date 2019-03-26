5190373eab754f1cd66b998b7c86b62ffa7916d0
nopops <nopops@nightofpassion.se>
2019-03-26 19:43:11 +0100

    removing empty lines

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import AppRoutes from 'views/Components/AppRoutes.jsx'
-
+import ReactGA from 'react-ga'
 
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 
-
-
-import ReactGA from 'react-ga'
-
 ReactGA.initialize('UA-134177845-1')
-
-
 
 ReactDOM.render(
   <AppRoutes/>,
