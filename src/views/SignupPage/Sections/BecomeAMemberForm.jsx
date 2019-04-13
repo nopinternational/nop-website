e1da5319b9d6780f29b39c076dc4d1c9d0bd82cd
nopops <nopops@nightofpassion.se>
2019-04-13 12:28:27 +0200

    adding auto email to signups

--- src/views/SignupPage/Sections/BecomeAMemberForm.jsx
+++ src/views/SignupPage/Sections/BecomeAMemberForm.jsx
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 // @material-ui/icons
 import Email from "@material-ui/icons/Email";
 import People from "@material-ui/icons/People";
 import Message from "@material-ui/icons/Message";
 
 // core components
+
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import Button from "components/CustomButtons/Button.jsx";
@@ -17,6 +24,8 @@ import productStyle from "assets/jss/material-kit-react/views/landingPageSection
 
 import firebase from 'firebase/app';
 import 'firebase/database';
+import sgMail from '@sendgrid/mail';
+
 class AboutNoP extends React.Component {
 
   state = {
@@ -26,7 +35,7 @@ class AboutNoP extends React.Component {
     email: '',
     message: ''
   };
-
+ 
   componentDidMount() {
     const   config = {
       apiKey: process.env.REACT_APP_FIREBSE_APIKEY,
@@ -34,14 +43,20 @@ class AboutNoP extends React.Component {
       databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
       projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
       storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
     };
 
     if (!firebase.apps.length) {
       firebase.initializeApp(config);
-      this.fb_database = firebase.database();
+      
     }
+    this.fb_database = firebase.database();
+    this.templateId = process.env.REACT_APP_SENDGRID_WELCOME_TEMPLATEID
   }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   handleNameChange = (event) => {
     this.setState({name: event.target.value})
@@ -56,11 +71,39 @@ class AboutNoP extends React.Component {
   };
 
   handleBecomeMember = () => {
-    this.setState({
-      open: false,
-    });
-    this.writeUserData (this.state.email,this.state.email,this.state.message);
+    const email = this.state.email;
+    if (!this.validateEmail(email)) {
+      this.setState({
+        open: true,
+      });
+      return
+    }
+    this.setState({ open: true });
+    this.writeUserData (this.state.name,this.state.email,this.state.message);
+    this.sendWelcomeMail(this.state.email,this.state.name);
   };
+
+  validateEmail = (email)  => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+
+  }
+  sendWelcomeMail = (mailTo, name) => {
+    sgMail.setApiKey(process.env.REACT_APP_SENDGRID_APIKEY);
+    
+    const msg = {
+      to: mailTo,
+      from: {
+        name: 'Night of Passion',
+        email: 'fest@nightofpassion.se',
+      },
+      subject: 'Night of Passion - Välkommen',
+      text: 'Night of Passion - Välkommen',
+      html: '<strong>Night of Passion - Välkommen</strong>',
+      templateId: this.templateId,
+    };
+    sgMail.send(msg);
+  }
 
   writeUserData = (name, email, message) => {
     this.fb_database.ref('users').push().set({
@@ -85,12 +128,13 @@ class AboutNoP extends React.Component {
           <GridItem xs={12} sm={12} md={8}>
             <h2 className={classes.title}>Bli medlem</h2>
             <h5 className={classes.description}>
-            Tack så mycket, nu är du med :)
+            Tack så mycket, vi kommer strax att skicka ett mail till er. 
+            (Får ni inget mail så kolla en extra gång i spam-foldern)
             </h5>
             <Link to={"/"} className={classes.link}>
               <Button
               color="primary">
-                OK
+                OK 
               </Button>
             </Link>
           </GridItem>
@@ -138,7 +182,8 @@ class AboutNoP extends React.Component {
                 }}
                 inputProps={{
                   onChange: this.handleEmailChange,
-                  type: "email",
+                  type: "email",                  
+                  autoComplete: "email",
                   endAdornment: (
                     <InputAdornment position="end">
                       <Email className={classes.inputIconsColor} />
@@ -168,39 +213,24 @@ class AboutNoP extends React.Component {
             </form>
           </GridItem>
         </GridContainer>
-          {/*
-          <div>
-            <GridContainer>
-              <GridItem xs={12} sm={12} md={4}>
-                <InfoArea
-                  title="Free Chat"
-                  description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-                  icon={Chat}
-                  iconColor="info"
-                  vertical
-                />
-              </GridItem>
-              <GridItem xs={12} sm={12} md={4}>
-                <InfoArea
-                  title="Verified Users"
-                  description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-                  icon={VerifiedUser}
-                  iconColor="success"
-                  vertical
-                />
-              </GridItem>
-              <GridItem xs={12} sm={12} md={4}>
-                <InfoArea
-                  title="Fingerprint"
-                  description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-                  icon={Fingerprint}
-                  iconColor="danger"
-                  vertical
-                />
-              </GridItem>
-            </GridContainer>
-          </div>
-          */}
+        <Dialog
+              open={this.state.open}
+              onClose={this.handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Felaktig epost"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Eposten är på ett ogiltigt format.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleClose} color="primary" autoFocus>
+                  OK
+                </Button>
+              </DialogActions>
+            </Dialog>
         </div>
       );
     }
