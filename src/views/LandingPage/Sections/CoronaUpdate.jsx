4698d868650314833b2a7d11ad3f745763004576
nopops <nopops@nightofpassion.se>
2020-03-15 19:36:49 +0100

    text update

--- src/views/LandingPage/Sections/CoronaUpdate.jsx
+++ src/views/LandingPage/Sections/CoronaUpdate.jsx
@@ -19,13 +19,20 @@ class SaveTheDate extends React.Component {
           <GridItem xs={12} sm={12} md={8}>
             <h2 className={classes.title}>Vi pausar våra träffar pga Coronaviruset</h2>
             <h5 className={classes.description}>
-              Hela Sverige och världen är påverkat av spridningen av Coronaviruset, även vi. 
-              Fram tills att det allmänna läget kring situationen pga viruset så kommer vi att pausa alla våra träffar. 
-              I nuläget så känns det inte helt rätt att arrangera träffar när istället regeringen och vetenskapen istället förordar att vi ska undvika allmänna sammankomster. </h5>
+            Hela Sverige och världen är påverkade av spridningen av Coronaviruset, vilket såklart även gäller oss. 
+            Fram tills att det allmänna läget kring situationen lugnat ned sig så kommer vi att pausa våra aktiviteter. 
+            I nuläget så känns det inte helt rätt att arrangera träffar när regeringen och vetenskapen istället förordar att vi ska undvika allmänna sammankomster.</h5>
             <h5 className={classes.description}>
             Så snart vi känner att det är läge för det så kommer vi naturligtvis igång med vår versamhet igen!
-            Hoppas att ni har förståelse för detta och vi hoppas att vi får chansen att ses snart efter att Corona-stormen har blåst över.</h5>
+            Hoppas att ni har förståelse för detta och vi hoppas att vi får chansen att ses snart efter att Corona-stormen har blåst över.
+            </h5>
+            <h5 className={classes.description}>
+              Ha det så mysigt med varandra så länge ;)
+              </h5>
 
+              <h5 className={classes.description}>
+              PS. Även om vi inte arrangerar några träffar för tillfället så går forfarande bra att anmäla sig för att bli medlemmar, se nedan.
+              </h5>
           </GridItem>
         </GridContainer>
       </div>
