6a03b257398bd28d5c0c3008bf7c80ec43aa0896
nopops <nopops@nightofpassion.se>
2020-04-01 17:18:13 +0200

    introducing prettier

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,60 +1,58 @@
-import React from "react";
+import React from 'react';
 
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from '@material-ui/core/styles/withStyles';
 
 // @material-ui/icons
 
 // core components
-import Header from "components/Header/Header.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
-import SnackbarContent from "components/Snackbar/SnackbarContent.jsx";
+import Header from 'components/Header/Header.jsx';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem.jsx';
+import HeaderLinks from 'components/Header/HeaderLinks.jsx';
+import Parallax from 'components/Parallax/Parallax.jsx';
+import SnackbarContent from 'components/Snackbar/SnackbarContent.jsx';
 
-
-import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+import landingPageStyle from 'assets/jss/material-kit-react/views/landingPage.jsx';
 
 // Sections for this page
-import AboutNoP from "./Sections/AboutNoP.jsx";
-import SaveTheDate from "./Sections/SaveTheDate.jsx";
-import BecomeAMember from "./Sections/BecomeAMember.jsx";
-import ThreeBenefitsOfNoP from "./Sections/ThreeBenefitsOfNoP.jsx";
-import WhosBehind from "./Sections/WhosBehind.jsx";
-import AboutSpringPartyTeaser from "./Sections/AboutSpringPartyTeaser.jsx";
-import CoronaUpdate from "./Sections/CoronaUpdate.jsx";
+import AboutNoP from './Sections/AboutNoP.jsx';
+import SaveTheDate from './Sections/SaveTheDate.jsx';
+import BecomeAMember from './Sections/BecomeAMember.jsx';
+import ThreeBenefitsOfNoP from './Sections/ThreeBenefitsOfNoP.jsx';
+import WhosBehind from './Sections/WhosBehind.jsx';
+import AboutSpringPartyTeaser from './Sections/AboutSpringPartyTeaser.jsx';
+import CoronaUpdate from './Sections/CoronaUpdate.jsx';
 
-import FallPartyTeaser from "./Sections/FallPartyTeaser.jsx";
-
-
+import FallPartyTeaser from './Sections/FallPartyTeaser.jsx';
 
 const dashboardRoutes = [];
 
 class LandingPage extends React.Component {
-
-
-
   render() {
     const { classes, ...rest } = this.props;
-    const emailLinkStyle = { color: "#FFFFFF" }
+    const emailLinkStyle = { color: '#FFFFFF' };
 
     const showMsg = false;
-    const msg = () => showMsg? (
-      <SnackbarContent
-      message={
-          <span>
-            Vi har skickat ut mail till alla medlemmar med info om mingelträffen och höstfesten. 
-            Tyvärr har vi problem med att nå medlemmar med @hotmail.com, @msn.com, @live.se, @outlook.com. 
-            All info om träffarna finner ni på sidorna här, annars maila oss på <a style={emailLinkStyle} href="mailto:fest@nightofpassion.se">fest@nightofpassion.se</a>
-          </span>
-      }
-      color="primary"
-      icon="info"
-    />
-    ): null;
+    const msg = () =>
+      showMsg ? (
+        <SnackbarContent
+          message={
+            <span>
+              Vi har skickat ut mail till alla medlemmar med info om mingelträffen och höstfesten. Tyvärr har vi problem
+              med att nå medlemmar med @hotmail.com, @msn.com, @live.se, @outlook.com. All info om träffarna finner ni
+              på sidorna här, annars maila oss på{' '}
+              <a style={emailLinkStyle} href="mailto:fest@nightofpassion.se">
+                fest@nightofpassion.se
+              </a>
+            </span>
+          }
+          color="primary"
+          icon="info"
+        />
+      ) : null;
 
     return (
       <div>
@@ -66,19 +64,16 @@ class LandingPage extends React.Component {
           fixed
           changeColorOnScroll={{
             height: 100,
-            color: "white"
+            color: 'white',
           }}
           {...rest}
         />
-        <Parallax filter image={require("assets/img/zero.jpg")}>
+        <Parallax filter image={require('assets/img/zero.jpg')}>
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
                 <h1 className={classes.title}>Night of Passion</h1>
-                <h4>
-                  Socialt. Passion. Sex.
-                </h4>
-
+                <h4>Socialt. Passion. Sex.</h4>
               </GridItem>
             </GridContainer>
           </div>
@@ -87,13 +82,13 @@ class LandingPage extends React.Component {
           <div className={classes.container}>
             {msg()}
             <AboutNoP />
-            <ThreeBenefitsOfNoP />            
+            <ThreeBenefitsOfNoP />
             <CoronaUpdate />
             <AboutSpringPartyTeaser />
             <BecomeAMember />
             <WhosBehind />
 
-          {/*
+            {/*
             
             <TeamSection />
             <WorkSection />
