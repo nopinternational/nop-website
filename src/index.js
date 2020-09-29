a1927adb7bf5ff0baadb41f2e5c12474322c3c2a
nopops <nopops@nightofpassion.se>
2020-09-29 21:18:41 +0200

    updating npm packages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppRoutes from 'views/Components/AppRoutes'
-import ReactGA from 'react-ga'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoutes from 'views/Components/AppRoutes';
+import ReactGA from 'react-ga';
 import Firebase, { FirebaseContext } from './components/Firebase';
-import "assets/scss/material-kit-react.scss?v=1.4.0";
+import 'assets/scss/material-kit-react.scss?v=1.4.0';
 
-ReactGA.initialize('UA-134177845-1')
+ReactGA.initialize('UA-134177845-1');
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
-    <AppRoutes/>
+    <AppRoutes />
   </FirebaseContext.Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
