33484cb809edc0e523351259d6ddb3e66898a56d
nopops <nopops@nightofpassion.se>
2020-10-24 15:25:58 +0200

    reading article list from CTF and creating list with links to each one of them

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery } from 'gatsby';
 
 import Layout from '../componentsG/layout';
 import SEO from '../componentsG/seo';
 
-const ArticlePage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-);
+const ArticlePage = () => {
+  const data = useStaticQuery(
+    graphql`
+      query MyQuery {
+        allContentfulArticle(filter: { slug: { ne: null } }) {
+          edges {
+            node {
+              id
+              slug
+              title
+            }
+          }
+        }
+      }
+    `
+  );
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+      <ul className="posts">
+        {data.allContentfulArticle.edges.map((edge) => {
+          return (
+            <li key={edge.node.id}>
+              <h2>
+                <Link to={`/article/${edge.node.slug}/`}>{edge.node.title}</Link>
+              </h2>
+            </li>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
+};
 
 export default ArticlePage;
