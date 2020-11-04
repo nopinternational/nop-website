7c5dc57a405bf54c9a33e87c4bd2880c6820b0d8
nopops <nopops@nightofpassion.se>
2020-11-04 22:36:19 +0100

    added gatsby-source-contentful, config and very basic impl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,36 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   pathPrefix: `/mini-gatsbyv2-material-kit-react`,
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Gatsby Default Starter",
   },
   plugins: [
-    'gatsby-plugin-resolve-src',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-sass',
-    'gatsby-plugin-material-ui',
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-sass",
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.REACT_APP_CTF_SPACEID,
+        accessToken: process.env.REACT_APP_CTF_CDN,
+      },
+    },
+    "gatsby-plugin-material-ui",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/assets/img/favicon.png", // This path is relative to the root of the site.
       },
     },
   ],
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -25,11 +25,28 @@ import ProductSection from "./Sections/ProductSection.jsx";
 import TeamSection from "./Sections/TeamSection.jsx";
 import WorkSection from "./Sections/WorkSection.jsx";
 
+import { useStaticQuery, graphql } from "gatsby"
+
 const dashboardRoutes = [];
 
-class LandingPage extends React.Component {
-  render() {
-    const { classes, ...rest } = this.props;
+const LandingPage = (props) => {
+//class LandingPage extends React.Component {
+
+
+  
+
+    const data = useStaticQuery(
+      graphql`
+        query {
+          contentfulPage(slug: {eq: "/"}) {
+            name
+            slug
+          }
+        }
+      `
+    )
+
+    const { classes, ...rest } = props;
     return (
       <div>
         <Header
@@ -48,7 +65,7 @@ class LandingPage extends React.Component {
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
-                <h1 className={classes.title}>Your Story Starts With Us.</h1>
+                <h1 className={classes.title}>--{data.contentfulPage.name}-- Your Story Starts With Us.</h1>
                 <h4>
                   Every landing page needs a small description after the big
                   bold title, that's why we added this text here. Add here all
@@ -81,6 +98,6 @@ class LandingPage extends React.Component {
       </div>
     );
   }
-}
+
 
 export default withStyles(landingPageStyle)(LandingPage);
